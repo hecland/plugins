ea99dd067c6196c15197872bd13a3f62ccd4b311
hecland <hecland@qq.com>
2019-12-10 15:27:26 +0800

    调整 scrollspy.js

--- src/scrollspy.js
+++ src/scrollspy.js
@@ -25,8 +25,6 @@ LazyScript.load('jquery', 'underscore', function(global){
   }
 
   function Scrollspy(options) {
-    // 被监听的容器
-    var _$container = $(window);
 
     // 待检测元素
     var _$anchors = $();
@@ -34,19 +32,134 @@ LazyScript.load('jquery', 'underscore', function(global){
     // 接收反馈的锚点链接
     var _$targets = $();
 
-    // 设置感测器
+    // 使用 options 提取参数
+    if (isSelector(options) || options === document || options === window) {
+      options = {
+        container: options
+      }
+
+      _$container = $(options);
+
+    } else if (! isObject(options)) {
+      options = {}
+
+      // // 提取容器信息
+      // if (options.container) {
+      //   _$container = $(options.container);
+      // }
+
+      // if (options.method) {
+      //   _method = options.method
+      // }
+    }
+
+    // 被监听的容器
+    var _$container = $(options.container || window);
+
+    // 确保窗口有效
+    if (! _$container.length || !(_$container[0] === window || _$container[0] instanceof HTMLElement)) {
+      _$container = $(window);
+    }
+
+    var _isWindow = _$container[0] === window;
+
+    // 获取容器的 BoundingRect
+    var _getContainerRect = (function() {
+      if (_isWindow) {
+        return function() {
+          var width = _$container.width();
+          var height = _$container.height();
+          return {
+            x: 0,
+            y: 0,
+            width: width,
+            height: height,
+            top: 0,
+            right: width,
+            bottom: height,
+            left: 0,
+          }
+        }
+      }
+      return function() {
+        return _$container[0].getBoundingClientRect()
+      }
+    })();
+
+    // 设置检测器
     var _sensor = {
-      position: .2, // 感测器位置（相对于容器顶部）
-      unit: '%',    // 感测器位置单位，'px' 或 '%'
+      position: .2, // 检测器位置（相对于容器顶部）
+      unit: '%',    // 检测器位置单位，'px' 或 '%'
       current: null,
     };
 
-    // 感测器检测『当前元素』方法
-    _sensor.getCurrent = function() {
-      var threshold = this.getPosition();
+    // 从 options 提取检测线位置值，以及确定值的单位
+    if (options.offset) {
+      _sensor.position = parseFloat(options.offset)
+      if (isNaN(_sensor.position)) {
+        _sensor.position = .2
+        _sensor.unit = '%'
+      } else {
+        if (options.offset.substr(-1) === '%') {
+          _sensor.position /= 100
+          _sensor.unit = '%'
+        } else {
+          _sensor.position = Math.round(_sensor.position)
+          _sensor.unit = 'px'
+        }
+      }
+    }
+
+    // 根据滚动窗口、检测线位置值单位、检测线位置值正负三个特征，确定获取检测线位置的方法
+    _sensor.getPosition = (function() {
+      if (_sensor.unit === '%') {
+        if (_sensor.position >= 0) {
+          return function() {
+            var containerRect = _getContainerRect();
+            return containerRect.top + containerRect.height * this.position
+          }
+        } else {
+          return function() {
+            var containerRect = _getContainerRect();
+            return containerRect.top + containerRect.height * (1 - this.position)
+          }
+        }
+      } else {
+        if (_sensor.position >= 0) {
+          return function() {
+            var containerRect = _getContainerRect();
+            return containerRect.top + this.position
+          }
+        } else {
+          return function() {
+            var containerRect = _getContainerRect();
+            return containerRect.top + containerRect.height - this.position
+          }
+        }
+      }
+    })();
+
+    // 检测一个元素与容器的交叉百分比
+    function _getIntersection(anchor) {
+      var rect = anchor.getBoundingClientRect();
+      var containerRect = _getContainerRect();
+      if (rect.bottom <= containerRect.top || rect.top >= containerRect.bottom) {
+        return 0
+      }
+      var top = Math.max(rect.top, containerRect.top);
+      var bottom = Math.min(rect.bottom, containerRect.bottom);
+      return (bottom - top) / rect.height
+    };
+
+    // 使用 near 方式获取『当前元素』
+    function _getNearAnchor() {
+      var threshold = _sensor.getPosition();
       var current = null, currentDistance = 999999;
       for (var index = 0, len = _$anchors.length; index < len; index++) {
         var anchor = _$anchors[index];
+        if (_getIntersection(anchor) <= 0) {
+          continue
+        }
         var rect = anchor.getBoundingClientRect();
         var distance = Math.abs(threshold - (rect.top + rect.height/2));
         if (distance < currentDistance) {
@@ -57,105 +170,39 @@ LazyScript.load('jquery', 'underscore', function(global){
       return current
     };
 
-    // 使用 options 提取参数
-    if (isSelector(options) || options === document || options === window) {
-
-      _$container = $(options);
-
-    } else if (isObject(options)) {
-
-      // 提取容器信息
-      if (options.container) {
-        _$container = $(options.container);
-      }
-
-      // 提取感测线位置值，以及确定值的单位
-      if (options.offset) {
-        _sensor.position = parseFloat(options.offset)
-        if (isNaN(_sensor.position)) {
-          _sensor.position = .2
-          _sensor.unit = '%'
-        } else {
-          if (options.offset.substr(-1) === '%') {
-            _sensor.position /= 100
-            _sensor.unit = '%'
-          } else {
-            _sensor.position = Math.round(_sensor.position)
-            _sensor.unit = 'px'
+    // 使用 strike 方式获取『当前元素』
+    function _getStrikeAnchor() {
+      var threshold = _sensor.getPosition();
+        for (var index = 0, len = _$anchors.length; index < len; index++) {
+          var anchor = _$anchors[index];
+          var rect = anchor.getBoundingClientRect();
+          if (threshold >= rect.top && threshold <= rect.top + rect.height) {
+            return anchor;
           }
         }
-      }
+        return null
+    };
 
-      if (options.method === 'strike') {
+    // 确定检测『当前元素』的方法
+    switch(options.method) {
+      case 'strike':
         _sensor.getCurrent = function() {
-          var threshold = this.getPosition();
-          for (var index = 0, len = _$anchors.length; index < len; index++) {
-            var anchor = _$anchors[index];
-            var rect = anchor.getBoundingClientRect();
-            if (threshold >= rect.top && threshold <= rect.top + rect.height) {
-              return anchor;
-            }
-          }
-          return null
-        };
-      }
-    }
-
-    // 确保窗口有效
-    if (! _$container.length || !(_$container[0] === window || _$container[0] instanceof HTMLElement)) {
-      _$container = $(window);
-    }
-
-    var _isWindow = _$container[0] === window;
-
-    // 根据滚动窗口、感测线位置值单位、感测线位置值正负三个特征，确定获取感测线位置的方法
-    _sensor.getPosition = (function() {
-      if (_isWindow) {
-        if (_sensor.unit === '%') {
-          if (_sensor.position >= 0) {
-            return function() {
-              return _$container.height() * this.position
-            }
-          } else {
-            return function() {
-              return _$container.height() * (1 - this.position)
-            }
-          }
-        } else {
-          if (_sensor.position >= 0) {
-            return function() {
-              return this.position
-            }
-          } else {
-            return function() {
-              return _$container.height() - this.position
-            }
-          }
+          return _getStrikeAnchor();
         }
-      } else {
-        if (_sensor.unit === '%') {
-          if (_sensor.position >= 0) {
-            return function() {
-              return _$container[0].getBoundingClientRect().top + _$container.height() * this.position
-            }
-          } else {
-            return function() {
-              return _$container[0].getBoundingClientRect().top + _$container.height() * (1 - this.position)
-            }
-          }
-        } else {
-          if (_sensor.position >= 0) {
-            return function() {
-              return _$container[0].getBoundingClientRect().top + this.position
-            }
-          } else {
-            return function() {
-              return _$container[0].getBoundingClientRect().top + _$container.height() - this.position
-            }
-          }
+        break;
+
+      case 'near':
+        _sensor.getCurrent = function() {
+          return _getNearAnchor();
         }
-      }
-    })();
+        break;
+
+      default:
+        _sensor.getCurrent = function() {
+          return _getStrikeAnchor() || _getNearAnchor();
+        }
+        break;
+    }
 
     // 设置反馈器
     var _feedback = {
@@ -226,7 +273,7 @@ LazyScript.load('jquery', 'underscore', function(global){
       update: function() {
         var current = _sensor.getCurrent();
 
-        if (current != _sensor.current) {
+        if (current !== _sensor.current) {
           _$targets.each(function(){ _feedback.deactive(this, _feedback.cls) })
           _sensor.current = current
         }
@@ -253,7 +300,7 @@ LazyScript.load('jquery', 'underscore', function(global){
         return this;
       },
 
-      // 查看感测器配置
+      // 查看检测器配置
       sensor: function() {
         return {
           position: _sensor.position,
